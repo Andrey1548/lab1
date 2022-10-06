d1d0b152f5207d5df5c426df9b09c8438b9dffe1
Andrey1548 <56449210+Andrey1548@users.noreply.github.com>
2022-10-06 19:32:01 +0300

    Update Lab1.purs

--- src/Lab1.purs
+++ src/Lab1.purs
@@ -1,23 +1,35 @@
 module Lab1 where
 
 import Prelude
-import Data.List (List(..))
-import Data.Maybe (Maybe(..))
+
+import Data.List (List(..), (:))
 import Effect (Effect)
 import Effect.Console (log)
 
+singleton :: forall a. a -> List a
+singleton a = Cons a Nil
 
-flip :: forall a b c. (a -> b -> c) -> b -> a -> c
-flip f x y = f y x
+null :: forall a. List a-> Boolean
+null Nil = true
+null _ = false
 
-toString :: Boolean -> String
-toString true = "true"
-toString _ = "false"
+reverse :: List ~> List
+reverse = go Nil
+  where
+  go acc Nil = acc
+  go acc (a : b) = go (a : acc) b
 
-head :: forall a. List a -> Maybe a
-head Nil = Nothing
-head (Cons x _) = Just x
+snoc :: forall a. List a -> a -> List a
+snoc Nil a = (Cons a Nil)
+snoc (Cons l a) b = reverse(Cons b(reverse (Cons l a)))
 
-test:: Effect Unit
+length :: forall a. List a -> Int
+length Nil = 0
+length (a : list) = length list + 1
+
+test::Effect Unit
 test = do
-  log $ show $ head (Cons "android" Nil)
+    log $ show $ singleton 3
+    log $ show $ null(Cons 2 (Cons 2 Nil))
+    log $ show $ snoc (1 : 2 : Nil) 3
+    log $ show $ length (1 : 2 : 3 : Nil)
